657a6e9765df908e7e972d56fa44864c27b15342
Ashwin42023 <ashwinchoyal520@gmail.com>
2023-12-20 14:43:39 +0530

    first commit

---
+++ pages/home/index.js
@@ -0,0 +1,10 @@
+import LandingPage from '@/component/landingPage'
+import React from 'react'
+
+function Home() {
+  return (
+    <LandingPage/>
+  )
+}
+
+export default Home;
---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+import LandingPage from '@/component/landingPage';
+import React from 'react'
+
+function Login() {
+  return (
+   <LandingPage/>
+  )
+}
+
+export default Login;
