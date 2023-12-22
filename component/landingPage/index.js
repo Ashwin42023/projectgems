2249f38f1906ea25cc89b58962dd1bc9d376ad34
Ashwin42023 <ashwinchoyal520@gmail.com>
2023-12-22 15:49:42 +0530

    22/dec

--- component/landingPage/index.js
+++ component/landingPage/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Head from "next/head";
+import React from "react";
+import HeadPage from "../layout/headPage";
+import HEAD_TITLES from "@/utils/constants/titleConstants";
+import TopSection from "./topSection";
+import CounterSections from "./counterSections";
+import BecomeAnInstructor from "./becomeAnInstructor";
 
 function LandingPage() {
   return (
-    <div> Hi i am LandingPage</div>
-  )
+    <HeadPage title={HEAD_TITLES.home}>
+      <main>
+        <TopSection />
+        <CounterSections />
+        <BecomeAnInstructor />
+      </main>
+    </HeadPage>
+  );
 }
 
-export default LandingPage 
+export default LandingPage;
