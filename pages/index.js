de273398d649cbefd82703b8f78cc47d2fe6cff2
Ashwin42023 <ashwinchoyal520@gmail.com>
2023-12-26 14:01:57 +0530

    26dec

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import LandingPage from '@/component/landingPage'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return 
+  return (
+    <LandingPage/>
+  )
     
 }
